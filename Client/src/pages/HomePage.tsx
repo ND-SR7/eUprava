f53c7e04c5fb0e752e02965fe6cd6ca21cb9a955
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2024-05-13 12:01:08 +0200

    Added JWT service
    - Added service for decoding token provided by IdP
    - Added redirect from Home page to Login page if token is not present

--- Client/src/pages/HomePage.tsx
+++ Client/src/pages/HomePage.tsx
@@ -1,13 +1,26 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Shared/Button/Button";
 import HeadingStyled from "../components/Shared/Heading/Heading.styled";
+import decodeJwtToken from "../services/JwtService";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const [sub, setSub] = useState("");
 
+  useEffect(() => {
+    const token = localStorage.getItem("token") || "";
+    if (token === "") navigate("/");
+
+    let sub = decodeJwtToken(token).sub;
+    sub = sub.substring(0, sub.indexOf("@"))
+    setSub(sub);
+    // eslint-disable-next-line
+  }, []);
+  
   return (
     <>
-      <HeadingStyled>Welcome to eUprava</HeadingStyled>
+      <HeadingStyled>Welcome to eUprava, {sub}</HeadingStyled>
       <h3>Select the service you want to access</h3>
       <Button
         key="navMUP"
